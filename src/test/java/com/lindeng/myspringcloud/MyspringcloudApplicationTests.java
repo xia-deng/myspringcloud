9d0bc90aa7e008873d71bf06d075535055c6d19a
xia_deng@yeah.net <xia_deng@yeah.net>
2020-02-06 10:28:10 +0800

    init the spring-cloud config server

---
+++ src/test/java/com/lindeng/myspringcloud/MyspringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lindeng.myspringcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyspringcloudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
